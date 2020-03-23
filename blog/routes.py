56d82668dedeafc8caacee915f836998c9283829
ZhangHao <1510647999@qq.com>
2020-03-23 21:55:08 +0800

    bolg增加登录

--- blog/routes.py
+++ blog/routes.py
@@ -1,30 +1,31 @@
-# !/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-@Author         :  hzhang
-------------------------------------
-@File           :  routes.py
-@CreateTime     :  2020/3/20 11:27
-------------------------------------
-"""
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 
 from blog import app
+from blog.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'hzhang'}
+    user = {'username': 'zhanghao'}
+
     posts = [
         {
-            'author': {'username': '刘'},
-            'body': '这是模板模块中的循环例子～1'
-
+            'author': {'username': '张三'},
+            'body': '这是模板循环例子～1'
         },
         {
-            'author': {'username': '忠强'},
-            'body': '这是模板模块中的循环例子～2'
+            'author': {'username': '李四'},
+            'body': '这是模板循环例子～2'
         }
     ]
-    return render_template('index.html', title='hzhang', user=user, posts=posts)
+    return render_template('index.html', title='我的', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'用户的登录名是{form.username.data}，是否记住我{form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='登录', form=form)
